92efee9491377c052fd7d541a468824251b1caf7
Inmaculada Concepcion <inmaculadaconcepcionmeneses@gmail.com>
2025-03-27 16:35:30 +0100

    corrección de un import que se puso por la cara

--- src/GestorArchivos.java
+++ src/GestorArchivos.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-import javax.swing.border.StrokeBorder;
-
 public class GestorArchivos {
     private String carpetaSeleccionada;
     private String ficheroSeleccionado;
